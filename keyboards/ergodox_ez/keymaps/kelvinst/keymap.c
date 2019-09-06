f8a5b78a817c304085d34c40e9b2800ece1b250b
kelvinst <kelvin.stinghen@gmail.com>
2019-09-06 08:23:10 -0300

    Differentiating reversed layers

--- keyboards/ergodox_ez/keymaps/kelvinst/keymap.c
+++ keyboards/ergodox_ez/keymaps/kelvinst/keymap.c
@@ -165,21 +165,34 @@ uint32_t layer_state_set_user(uint32_t state) {
   ergodox_right_led_3_off();
   switch (layer) {
     case 2:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
+      ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
+      ergodox_right_led_3_set(LED_BRIGHTNESS_LO);
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+    case 2:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_1_on();
       break;
     case 3:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
       ergodox_right_led_1_on();
       break;
     case 4:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_2_on();
       break;
     case 5:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
       ergodox_right_led_2_on();
       break;
     case 6:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_3_on();
       break;
     case 7:
+      ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
       ergodox_right_led_3_on();
       break;
     default:
