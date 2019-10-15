528660cea7e40ff847b9f8eeda384bedd190ab00
kelvinst <kelvin.stinghen@gmail.com>
2019-10-15 15:45:43 -0300

    Fixing the maps

--- keyboards/ergodox_ez/keymaps/kelvinst/keymap.c
+++ keyboards/ergodox_ez/keymaps/kelvinst/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_SPC  , KC_BSPC , _______ ,           _______ , OSM_ALT , OSM_GUI
     ) , 
 
-  [2] = LAYOUT_ergodox(
+  [2] = LAYOUT_ergodox_pretty(
       KC_ESC  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , _______ ,                               _______ , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS ,
       KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , _______ ,                               _______ , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_EQL  ,
       KC_LCTL , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                                                   KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_BSLS ,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_SPC  , KC_ENT  , OSL(3)  ,           OSL(3)  , KC_BSPC , KC_SPC
     ) , 
 
-  [3] = LAYOUT_ergodox(
+  [3] = LAYOUT_ergodox_pretty(
       KC_MINS , KC_0    , KC_9    , KC_8    , KC_7    , KC_6    , _______ ,                               _______ , KC_5    , KC_4    , KC_3    , KC_2    , KC_1    , KC_ESC  ,
       KC_EQL  , KC_P    , KC_O    , KC_I    , KC_U    , KC_Y    , _______ ,                               _______ , KC_T    , KC_R    , KC_E    , KC_W    , KC_Q    , KC_TAB  ,
       KC_BSLS , KC_SCLN , KC_L    , KC_K    , KC_J    , KC_H    ,                                                   KC_G    , KC_F    , KC_A    , KC_S    , KC_D    , KC_LCTL ,
