09583ce762d7ae9184d324583174c67cf4989cdb
kelvinst <kelvin.stinghen@gmail.com>
2019-10-21 10:10:54 -0300

    Increasing mouse speed, screen is too big :joy:

--- keyboards/ergodox_ez/keymaps/kelvinst/config.h
+++ keyboards/ergodox_ez/keymaps/kelvinst/config.h
@@ -24,20 +24,20 @@
 
 #define MK_3_SPEED
 
-#define MK_C_OFFSET_0 4
+#define MK_C_OFFSET_0 5
 #define MK_C_INTERVAL_0 16
 #define MK_W_OFFSET_0 1	
 #define MK_W_INTERVAL_0 120
 
-#define MK_C_OFFSET_1 7	
+#define MK_C_OFFSET_1 10
 #define MK_C_INTERVAL_1 16
 #define MK_W_OFFSET_1 1
-#define MK_W_INTERVAL_1 80
+#define MK_W_INTERVAL_1 60
 
-#define MK_C_OFFSET_2 10
+#define MK_C_OFFSET_2 20
 #define MK_C_INTERVAL_2 16	
 #define MK_W_OFFSET_2 1
-#define MK_W_INTERVAL_2 60
+#define MK_W_INTERVAL_2 30
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                 Effects                                     *
